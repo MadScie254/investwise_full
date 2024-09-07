dbf52831e561f792a3a6af0dc3fdab66820c5d1c
MadScie254 <daniel.wanjala1@student.moringaschool.com>
2024-09-07 18:43:04 +0300

    new ipdate

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,36 @@
-const gulp = require('gulp'); 
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+// Compile Sass
+gulp.task('sass', function() {
+  return gulp.src('./assets/css/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
+});
+
+// Minify JS
+gulp.task('scripts', function() {
+  return gulp.src('./assets/js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
+});
+
+// Watch & Serve
+gulp.task('serve', function() {
+  browserSync.init({
+    server: './pages'
+  });
+
+  gulp.watch('./assets/css/**/*.scss', gulp.series('sass'));
+  gulp.watch('./assets/js/**/*.js', gulp.series('scripts'));
+  gulp.watch('./pages/**/*.html').on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', gulp.series('sass', 'scripts', 'serve'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
-const path = require('path'); 
+const path = require('path');
+
+module.exports = {
+  entry: './assets/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './pages',
+  },
+};
